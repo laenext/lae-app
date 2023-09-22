026d64044bb74d66a105cf06eeb85b6e5e92c56f
shasgit <shas.bn96@gmail.com>
2023-09-22 16:45:55 +0200

    Initial commit
    Initial commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.text())
+      .then(setGreeting);
+  }, []);
+
+  return (
+    <>
+      {greeting} {"Test"}
+    </>
+  );
+}
+
+export default App;
